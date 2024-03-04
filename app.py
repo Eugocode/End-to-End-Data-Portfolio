a777eaf70a2d4a1b244faefdd8c70133b78418af
Eugocode <dmcawaling@up.edu.ph>
2024-03-04 15:29:27 +0800

    add documentations

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-import sys  #add parameters when we run script
+import sys  #used to add parameters when we run scripts/functions
 import json #for configuration file
+import time #for working with time-related functions
+import schedule #schedule execution
+import pandas as pd #preprocessing of data
+from os import environ, remove #access os environment variables and file deletion
+from pathlib import Path #get the actual file system paths
+from ftplib import FTP_TLS #to connect to FTP Server
 
-# schedule execution
-import time 
-import schedule
+#---------------------------------------------------START OF: Extracting Data from Web to FTP --------------------------------------------------###
 
-# preprocessing of data
-import pandas as pd 
-# access environment variables
-from os import environ, remove 
-# get the actual path
-from pathlib import Path 
-# to connect to FTP Server
-from ftplib import FTP_TLS 
-
+# Function to retrieve the authenticated FTP_TLS object
 def get_ftp() -> FTP_TLS:
     # Get FTP details
     FTPHOST = environ["FTPHOST"]
@@ -21,36 +17,38 @@ def get_ftp() -> FTP_TLS:
     FTPPASS = environ["FTPPASS"]
     FTPPORT = environ["FTPPORT"]
 
-    # Return authenticated FTP
+    # Return authenticated FTP object
     ftp = FTP_TLS(FTPHOST, FTPUSER, FTPPASS)
-
     ftp.prot_p()
 
     print("FTP server running...")
 
     return ftp 
 
+# Function to upload file to FTP server
 def upload_to_ftp(ftp: FTP_TLS, file_source: Path):
     with open(file_source, "rb") as fp:
         ftp.storbinary(f"STOR {file_source.name}", fp)
 
+# Function to delete file from local filesystem
 def delete_file(file_source: str | Path):
     remove(file_source)
 
+# Function to read CSV data from web url
 def read_csv(config: dict) -> pd.DataFrame:
     url = config["URL"]
     params = config["PARAMS"]
     return pd.read_csv(url, **params) #** means that we are decompressing the dictionary and get the actual parameter name
 
+# Main pipeline function for data extraction and upload
 def pipeline():
-       # Load source configuration
+    # Load source configuration
     with open("config.json", "rb") as fp:
         config = json.load(fp)
     
     # Get an authenticated FTP Object
     ftp = get_ftp()
 
-    # print(read_csv(config["OFAC_SDN"]).head())
     for source_name, source_config in config.items():
         file_name = Path(source_name + ".csv")
         df = read_csv(source_config)
@@ -65,16 +63,19 @@ def pipeline():
         delete_file(file_name)
         print(f"File {file_name} has been deleted.")
 
+### ---------------------------------------------------- END OF: Extracting Data from Web to FTP -----------------------------------------------###
+
 if __name__=="__main__":
     
+    # add 2 parameters (manual and schedule) when running the script
     param = sys.argv[1]
 
     if param=="manual":
         pipeline()
-        
+
     elif param=="schedule":
 
-        # scheduling to run at a certain time
+        # scheduling to run everyday at a certain time
         schedule.every().day.at("14:18").do(pipeline)
 
         while True:
